a39cab025688903233f21a08dd7a240b942f02cd
aliprg0 <aliprg0@gmail.com>
2022-03-12 15:58:52 +0330

    test-5

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:flask_app
+web: python app.py
--- app.py
+++ app.py
@@ -2,23 +2,22 @@ from flask import Flask, request, render_template, jsonify
 from utils import *
 
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 __model = None
 
 
-@flask_app.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-
-@flask_app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
    try:
-    data = request.files["myFile"]
-    data.save(f"data/{data.filename}")
-    result = predict_the_pic(f"data/{data.filename}", model=__model)
-    result = make_final_result(result=result)
-    return jsonify({"Result": result})
+     data = request.files["myFile"]
+     data.save(f"data/{data.filename}")
+     result = predict_the_pic(f"data/{data.filename}", model=__model)
+     result = make_final_result(result=result)
+     return jsonify({"Result": result})
    except:
       return jsonify({"Result" : "An Error Occurred"})
 
@@ -27,4 +26,4 @@ if __name__ == "__main__":
 
     __model = return_model()
 
-    flask_app.run(host='0.0.0.0', port=5000)
+    app.run()
